a3ca40fe203b35d3b2cd202a839b45281b26506a
Hunter Gongloff <hgongloff12@gmail.com>
2022-10-23 14:54:21 -0400

    updated speaker code

--- bin/sensors/speaker.py
+++ bin/sensors/speaker.py
@@ -3,34 +3,30 @@ from time import sleep
 
 class Speaker:
 
-    def __init__(self):
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(23, GPIO.OUT)
-        pin23 = GPIO.PWM(23, 100)
-        pin23.start(50)
+    def __init__(self, pin):
+        self.pin = pin
 
     def play_alarm(self):
         GPIO.output(23, GPIO.HIGH)
-        pin23.ChangeFrequency(16.35) # C0
+        self.pin.ChangeFrequency(16.35) # C0
         sleep(1)
-        pin23.ChangeFrequency(261.63) # C4
+        self.pin.ChangeFrequency(261.63) # C4
         sleep(1)
-        pin23.ChangeFrequency(293.66) # D4
+        self.pin.ChangeFrequency(293.66) # D4
         sleep(1)
-        pin23.ChangeFrequency(329.63) # E4
+        self.pin.ChangeFrequency(329.63) # E4
         sleep(1)
-        pin23.ChangeFrequency(349.23) # F4
+        self.pin.ChangeFrequency(349.23) # F4
         sleep(1)
-        pin23.ChangeFrequency(392.00) # G4
+        self.pin.ChangeFrequency(392.00) # G4
         sleep(1)
-        pin23.ChangeFrequency(440.00) # A4
+        self.pin.ChangeFrequency(440.00) # A4
         sleep(1)
-        pin23.ChangeFrequency(493.88) # B4
+        self.pin.ChangeFrequency(493.88) # B4
         sleep(1)
-        pin23.ChangeFrequency(523.25) # A5
+        self.pin.ChangeFrequency(523.25) # A5
         sleep(1.5)
-        pin23.ChangeFrequency(16.35) # C0
+        self.pin.ChangeFrequency(16.35) # C0
         sleep(1)
         GPIO.output(23, GPIO.LOW)
         sleep(1)
