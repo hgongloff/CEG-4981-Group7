030dead93afeddeb3108c72479d037c5aa750d73
Hunter Gongloff <hgongloff12@gmail.com>
2022-10-29 18:23:42 -0400

    updated camera/video

--- sensor-examples/camera code/camera.py
+++ sensor-examples/camera code/camera.py
@@ -1,43 +1,9 @@
-import numpy as np
-import cv2
-import v4l2py
-import time
-import subprocess
-cap = cv2.VideoCapture(0) 
-while(True):
-    # Capture frame-by-frame
-    subprocess.call(['v4l2-ctl -d /dev/video0 -c ircut=0'],shell=True)
-    time.sleep(0.1)
-    ret, frame = cap.read()
-    cv2.imshow('Arducam ar1335 ircut=0',0)
-    subprocess.call(['v4l2-ctl -d /dev/video0 -c ircut=1'],shell=True)
-    time.sleep(0.1)
-    ret, frame2 = cap.read()
-    cv2.imshow('Arducam ar1335 ircut=1',0)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        print("Saving image...")
-        cap.set(3,4160)  #set width
-        cap.set(4,3120)  #set height
-        time.sleep(0.1)
-        subprocess.call(['v4l2-ctl -d /dev/video0 -c ircut=1'],shell=True)
-        time.sleep(0.1)
-        print("ircut=1")
-        cap.grab()
-        cap.grab()
-        cap.grab()
-        ret, frame = cap.read()
-        cv2.imwrite("image_ircut_1.jpg",frame)
-        subprocess.call(['v4l2-ctl -d /dev/video0 -c ircut=0'],shell=True)
-        time.sleep(0.1)
-        print("ircut = 0")
-        cap.grab()
-        cap.grab()
-        cap.grab()
-        cap.grab()
-        cap.grab()
-        ret,frame2 = cap.read()
-        cv2.imwrite("image_ircut_0.jpg",frame2)
-        break   
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+import picamera as cam
+import time as t
+
+camera=cam.PiCamera()
+camera.resolution=(640,360)
+camera.start_preview()
+t.sleep(2)
+camera.capture("test.jpg")
+camera.close()
--- sensor-examples/camera code/camera2.py
+++ sensor-examples/camera code/camera2.py
@@ -1,25 +1,15 @@
-import numpy as np
+import picamera as cam
+import picamera.array as ar
 import cv2
-import v4l2py
-import time
-import subprocess
-cap = cv2.VideoCapture(0)
-while(True):
-    # Capture frame-by-frame
-    subprocess.call(['v4l2-ctl -d /dev/video0 -c ircut=0'],shell=True)
-    time.sleep(0.1)
-    ret, frame = cap.read()
-    cv2.imwrite("image_ircut_0.jpg",0)
-    cv2.imshow('Arducam ar1335 ircut=0',0)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+camera2=cam.PiCamera()
+camera2.resolution=(640,480)
+capturing_camera=ar.PiRGBArray(camera2,size=(640,480))
+
+for frame in camera2.capture_continuous(capturing_camera,format="bgr",use_video_port=True):
+    img=frame.array
+    cv2.imshow("Webcam",img)
+    capturing_camera.truncate(0)
+
+    if cv2.waitKey(10)==27:
+        
         break
-    subprocess.call(['v4l2-ctl -d /dev/video0 -c ircut=1'],shell=True)
-    time.sleep(0.1)
-    ret, frame2 = cap.read()
-    cv2.imwrite("image_ircut_1.jpg",0)
-    cv2.imshow('Arducam ar1335 ircut=1',0)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break   
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
